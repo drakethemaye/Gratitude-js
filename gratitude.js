9ecf5517b61a669b6d9543e06ac8e8c8801b4343
Rhys Chin <chinrhys@gmail.com>
2024-11-04 10:32:15 -0500

    comment

--- gratitude.js
+++ gratitude.js
@@ -1,1 +1,9 @@
 const gratitudeList = [ 'parents', 'friends', 'cute animals', 'the seasons changing']
+
+const displayGratitude = (list) => {
+    const gratitudeList = document.getElementById('gratitude-list');
+
+    for (index in list) {
+        
+    }
+}
